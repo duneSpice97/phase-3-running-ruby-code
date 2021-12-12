e25e9d176a4ea3172dadd8cf380255ce5be4c175
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2021-12-12 16:08:53 -0700

    commit tree 2e92896c64b1558d3bb40304437f81fa9399628f

---
+++ app.rb
@@ -0,0 +1,35 @@
+# # outputs string of text to the terminal
+
+# # puts "hello world!"
+# # puts "hello world!"
+# # puts "hello world!"
+
+# # print "hello world!"
+# # print "hello world!"
+# # print "hello world!"
+
+# # puts [1, 2, 3]
+
+# print [1, 2, 3]
+# print [1, 2, 3]
+# print [1, 2, 3]
+
+
+# p [1, 2, 3]
+# p [1, 2, 3]
+# p [1, 2, 3]
+
+
+# puts [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
+# # or use paranthesis 
+
+#  # enter $ run app.rb to ouput to terminal!
+
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1,2,3]
